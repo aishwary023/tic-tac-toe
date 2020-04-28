3ac87f8520fac23cc17876ad6379804234a75151
aishwary023 <aishwary.saxena.min19@itbhu.ac.in>
2020-04-28 08:54:55 +0530

    Unbeatable AI Bot added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function toStart() {
     document.getElementById("startPage").style.display = "flex";
 }
 let mode, mark;
+
 function toBoard() {
 
 
@@ -37,20 +38,23 @@ function toBoard() {
 
     if (mode === 'multiplayer')
         multiplayer();
+    else {
+        single();
+    }
 }
 
-const winCombi = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 function multiplayer() {
+    const winCombi = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
     console.log("inside multiplayer function");
 
     var restartbutton = document.getElementById("restartBtn");
-    restartbutton.addEventListener('click', function(){
+    restartbutton.addEventListener('click', function () {
         cellElements.forEach(cell => {
             cell.classList.remove("circle");
             cell.classList.remove("cross");
             cell.removeEventListener('click', handleClick)
-    
+
 
         });
     })
@@ -81,14 +85,13 @@ function multiplayer() {
         }
     }
 
-   function isDraw()
-   {
-    
+    function isDraw() {
+
         return [...cellElements].every(cell => {
-          return cell.classList.contains("cross") || cell.classList.contains("circle")
+            return cell.classList.contains("cross") || cell.classList.contains("circle")
         })
-    
-   }
+
+    }
 
     function endgame(draw) {
 
@@ -106,20 +109,199 @@ function multiplayer() {
 
         document.getElementById("endPage").style.display = "flex";
 
-       
+
 
     }
 
     function checkWin(mark) {
+
+
+        return ((winCombi.some(combination => {
+            return combination.every(index => {
+                return cellElements[index].classList.contains(mark)
+            })
+        })));
+
+
+
+    }
+}
+
+function single() {
+    const winCombi = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    console.log("inside single");
+    var winningmessage = document.getElementById("winMessage");
+    var restartbutton = document.getElementById("restartBtn");
+    restartbutton.addEventListener('click', function () {
+        cellElements.forEach(cell => {
+            cell.classList.remove("circle");
+            cell.classList.remove("cross");
+            cell.removeEventListener('click', handleClick)
+
+
+        });
+    })
+
+    const cellElements = document.querySelectorAll('[data-cell]');
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+
+
+    function handleClick(e) {
+        const cell = e.target;
+
+
+        humanTurn(mark, cell);
+        mark = mark == 'cross' ? 'circle' : 'cross';
+        console.log("mark exchanged after human turn");
+        botTurn(mark);
+        mark = mark == 'cross' ? 'circle' : 'cross';
+        console.log("mark exchanged after bot turn");
+
+    }
+
+    function humanTurn(mark, cell) {
+
+        cell.classList.add(mark);
+        console.log("added human response");
+        checkResultHuman(mark);
+
+
+    }
+
+    function botTurn(mark) {
+        let bestScore = -Infinity;
+        var index;
         for (var i = 0; i < 9; i++) {
+            if (cellElements[i].classList.value === 'cell') {
+                console.log("FOUND EMPTY");
 
-            return ((winCombi.some(combination => {
-                return combination.every(index => {
-                    return cellElements[index].classList.contains(mark)
-                })
-            })));
+                cellElements[i].classList.add(mark);
+                let score = minimax(cellElements, 0, false,mark);
+                cellElements[i].classList.remove(mark);
+                console.log(score)
+                if (score > bestScore) {
+                    bestScore = score;
+                    index = i;
+                    
+                }
 
+
+
+            }
+        }
+
+        cellElements[index].classList.add(mark);
+        checkResultBot(mark);
+    }
+
+
+    function minimax(cellElements, depth, isMaximizing,mark) {
+
+        if (((winCombi.some(combination => {
+            return combination.every(index => {
+                return cellElements[index].classList.contains(mark)
+            })
+        })))) {
+            return 1;
+        }
+        else if ([...cellElements].every(cell => {
+            return cell.classList.contains("cross") || cell.classList.contains("circle")
+        })) {
+            return 0;
+        }
+        else if (((winCombi.some(combination => {
+            return combination.every(index => {
+                return cellElements[index].classList.contains(mark == 'circle' ? 'cross' : 'circle')
+            })
+        }))))
+        return -1;
+
+        if (isMaximizing) {
+            let bestScore = -Infinity;
+            for (let i = 0; i < 9; i++) {
+
+                // Is the spot available?
+                if (cellElements[i].classList.value === 'cell') {
+                    cellElements[i].classList.add(mark);
+                    let score = minimax(cellElements, depth + 1, false,mark);
+                    cellElements[i].classList.remove(mark);
+
+                    bestScore = (score > bestScore) ? score : bestScore;
+                }
+
+            }
+
+            return bestScore;
+        }
+        else {
+            let bestScore = Infinity;
+            for (let i = 0; i < 9; i++) {
+
+                // Is the spot available?
+                if (cellElements[i].classList.value === 'cell') {
+                    cellElements[i].classList.add(mark == 'circle' ? 'cross' : 'circle');
+                    let score = minimax(cellElements, depth + 1, true,mark );
+                    cellElements[i].classList.remove(mark == 'circle' ? 'cross' : 'circle');
+
+                    bestScore = (score < bestScore) ? score : bestScore;
+                }
+
+            }
+
+            return bestScore;
+        }
+    }
+
+
+
+
+
+
+
+
+    function checkResultHuman(mark) {
+        if (checkWin(mark)) {
+            winningmessage.innerText = "YOU WIN!";
+            document.getElementById("endPage").style.display = "flex";
+        }
+        else if (isDraw(mark)) {
+            winningmessage.innerText = "DRAW";
+            document.getElementById("endPage").style.display = "flex";
         }
 
     }
+
+    function checkResultBot(mark) {
+        if (checkWin(mark)) {
+            winningmessage.innerText = "BOT WINs!";
+            document.getElementById("endPage").style.display = "flex";
+        }
+        else if (isDraw(mark)) {
+            winningmessage.innerText = "DRAW";
+            document.getElementById("endPage").style.display = "flex";
+        }
+    }
+
+    function checkWin(mark) {
+
+
+        return ((winCombi.some(combination => {
+            return combination.every(index => {
+                return cellElements[index].classList.contains(mark)
+            })
+        })));
+    }
+    function isDraw() {
+
+        return [...cellElements].every(cell => {
+            return cell.classList.contains("cross") || cell.classList.contains("circle")
+        })
+
+    }
+
+
+
+
 }
